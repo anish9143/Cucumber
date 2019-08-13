a1e6fa60ee3384963e9f3171b7ec7793f6a0f73f
Anish Thakur <48209887+anish9143@users.noreply.github.com>
2019-08-13 12:26:07 +0530

    Update Runner.java

--- Cucumber_Selenium_Integration/src/testRunner/Runner.java
+++ Cucumber_Selenium_Integration/src/testRunner/Runner.java
@@ -9,11 +9,11 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 //@RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "Features", 									// path of features file
-		glue = {"StepDefinitions" }, 							// path of step definition files
-		plugin= {"pretty","html:test-output","json:json_output\\cucumber.json"},			// to generate a report in various formats like in html format or json or any other format like junit xml can be generated
-		monochrome = true,											// to display console output in readabale format
-		dryRun = false, 													// to match feature with step methods whether for each steps we have annotated step methods available or not keep it false once checked
+		features = "Features", 			// path of features file
+		glue = {"StepDefinitions" }, 		// path of step definition files
+		plugin= {"pretty","html:test-output","json:json_output\\cucumber.json"}, // to generate a report in various formats like in html format or json or any other format like junit xml can be generated
+		monochrome = true,		       // to display console output in readabale format
+		dryRun = false, 			// to match feature with step methods whether for each steps we have annotated step methods available or not keep it false once checked
 		strict= true															// it strictly checks the mapping between feature file and stepdefinitions methods	and it gives Pending Exception if not followed
 		)
 
